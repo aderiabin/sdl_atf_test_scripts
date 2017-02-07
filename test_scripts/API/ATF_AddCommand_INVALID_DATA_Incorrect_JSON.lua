b74455390ed18f7e85df76e714680de8490e29d3
Andrii Pavliuk <arpavliuk@luxoft.com>
2017-02-07 18:02:56 +0200

    Use autoformating tool on files, remove redundat comments

--- test_scripts/API/ATF_AddCommand_INVALID_DATA_Incorrect_JSON.lua
+++ test_scripts/API/ATF_AddCommand_INVALID_DATA_Incorrect_JSON.lua
@@ -1,6 +1,7 @@
 ---------------------------------Required Shared Libraries-----------------------------------
 require('user_modules/all_common_modules')
 local consts = require('user_modules/consts')
+
 -------------------------------------Common Variables ---------------------------------------
 local app = config.application1.registerAppInterfaceParams
 
@@ -13,24 +14,24 @@ common_steps:PutFile("Precondition_Put_File", "action.png")
 -- In case:
 -- the request "AddCommand" comes to SDL with incorrect json syntax
 -- SDL must:
--- respond with resultCode "INVALID_DATA" and success:"false" value. 
-----------------------------------------------------------------------------------------------
+-- respond with resultCode "INVALID_DATA" and success:"false" value.
+
 function Test:AddCommand_INVALID_DATA_Incorrect_JSON()
   local rpc_service_type = 7
   local single_frame_frame_info_type = 0
   local request_rpc_type = 0
   local add_command_function_id = 5
   local incorrect_json_missing_colon_after_cmd_id = '{"cmdID" 55,"vrCommands":["synonym1","synonym2"],' ..
-    '"menuParams":{"position":1000,"menuName":"Item To Add"},"cmdIcon":{"value":"action.png","imageType":"DYNAMIC"}}'
+  '"menuParams":{"position":1000,"menuName":"Item To Add"},"cmdIcon":{"value":"action.png","imageType":"DYNAMIC"}}'
 
-  local msg = 
+  local msg =
   {
-    serviceType      = rpc_service_type,
-    frameInfo        = single_frame_frame_info_type,
-    rpcType          = request_rpc_type,
-    rpcFunctionId    = add_command_function_id,
+    serviceType = rpc_service_type,
+    frameInfo = single_frame_frame_info_type,
+    rpcType = request_rpc_type,
+    rpcFunctionId = add_command_function_id,
     rpcCorrelationId = self.mobileSession.correlationId,
-    payload          = incorrect_json_missing_colon_after_cmd_id
+    payload = incorrect_json_missing_colon_after_cmd_id
   }
   self.mobileSession:Send(msg)
 
