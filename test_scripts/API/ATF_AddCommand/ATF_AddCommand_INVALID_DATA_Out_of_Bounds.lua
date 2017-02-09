baa236be7b928f47c6022c8dcaccc3ac19e62369
Andrii Pavliuk <arpavliuk@luxoft.com>
2017-02-09 10:06:32 +0200

    Extract test case for INALID_DATA with menuParams and vrCommands missing
    Extracted test cases from ATF_AddCommand.lua with "INVALID_DATA" code
    when a request with both "menuParams" and "vrCommands" missing recieved to
    ATF_AddCommand_INVALID_DATA_Both_menuParams_and_vrCommands_Missing.
    
    Task: APPLINK-31634
    See also: APPLINK-19326

--- test_scripts/API/ATF_AddCommand/ATF_AddCommand_INVALID_DATA_Out_of_Bounds.lua
+++ test_scripts/API/ATF_AddCommand/ATF_AddCommand_INVALID_DATA_Out_of_Bounds.lua
@@ -1,10 +1,35 @@
+---------------------------------------------------------------------------------------------
+-- Requirements:
+-- [APPLINK-16110]: [GeneralResultCodes] INVALID_DATA out of bounds
+
+-- Description:
+-- In case the request comes to SDL with out-of-bounds array ranges or out-of-bounds
+-- parameters values (including parameters of the structures) of any type, SDL must respond
+-- with resultCode "INVALID_DATA" and success:"false" value.
+
+-- Preconditons:
+-- 1. Mobile application is registered and activated on HMI
+-- 2. Icon file is uploaded to HMI
+
+-- Performed steps
+-- 1. Application sends "AddCommand" request which contains parameter with out-of-bounds
+-- array ranges or out-of-bounds values (for a single parameter or as the part of the
+-- structure)
+
+-- Expected result:
+-- 1. SDL responds with resultCode:"INVALID_DATA" and success:"false" value
+
+-- Note:
+-- In every test case valid arguments (valid_args) are cloned to local variable then some
+-- value is changed to one that falls out of determined bounds then its value is passed to
+-- local SendAddCommandExpectResponse() function where "AddCommand" RPC is sent and response is
+-- checked.
 -------------------------------------Required Shared Libraries--------------------------------
 require('user_modules/all_common_modules')
 local consts = require('user_modules/consts')
 ------------------------------------ Common Functions ----------------------------------------
-local function SendRpcExpectResponse(self, rpc_args)
+local function SendAddCommandExpectResponse(self, rpc_args)
   local cid = self.mobileSession:SendRPC("AddCommand", rpc_args)
-
   self.mobileSession:ExpectResponse(cid, { success = false, resultCode = "INVALID_DATA" })
   :Timeout(consts.sdl_to_mobile_default_timeout)
 end
@@ -35,69 +60,56 @@ local valid_args = {
 common_steps:PreconditionSteps("Start_SDL_To_Activate_Application", 7)
 common_steps:PutFile("Precondition_Put_File", "icon.png")
 ------------------------------------------Tests-----------------------------------------------
--- This script covers the following requirement:
--- In case:
--- the request comes to SDL with out-of-bounds array ranges or out-of-bounds parameters
--- values (including parameters of the structures) of any type
--- SDL must:
--- respond with resultCode:"INVALID_DATA" and success:"false" value.
-
--- Note:
--- In every test case valid arguments (valid_args) are cloned to local variable then some
--- value is changed to one that falls out of determined bounds then its value is passed to
--- local SendRpcExpectResponse() function where "AddCommand" RPC is sent and response is
--- checked.
-----------------------------------------------------------------------------------------------
-function Test:AddCommand_cmdIDBelowMin()
+function Test:AddCommand_cmdID_BelowMin()
   local out_of_bounds_args = Clone(_, valid_args)
   out_of_bounds_args.cmdID = -1
-  SendRpcExpectResponse(self, out_of_bounds_args)
+  SendAddCommandExpectResponse(self, out_of_bounds_args)
 end
 
-function Test:AddCommand_cmdIDOverMax()
+function Test:AddCommand_cmdID_OverMax()
   local out_of_bounds_args = Clone(_, valid_args)
   out_of_bounds_args.cmdID = 2000000001
-  SendRpcExpectResponse(self, out_of_bounds_args)
+  SendAddCommandExpectResponse(self, out_of_bounds_args)
 end
 
-function Test:AddCommand_menuParamParentIDBelowMin()
+function Test:AddCommand_menuParamParentID_BelowMin()
   local out_of_bounds_args = Clone(_, valid_args)
   out_of_bounds_args.menuParams.parentID = -1
-  SendRpcExpectResponse(self, out_of_bounds_args)
+  SendAddCommandExpectResponse(self, out_of_bounds_args)
 end
 
-function Test:AddCommand_menuParamParentIDOverMax()
+function Test:AddCommand_menuParamParentID_OverMax()
   local out_of_bounds_args = Clone(_, valid_args)
   out_of_bounds_args.menuParams.parentID = 2000000001
-  SendRpcExpectResponse(self, out_of_bounds_args)
+  SendAddCommandExpectResponse(self, out_of_bounds_args)
 end
 
-function Test:AddCommand_menuParamPositonBelowMin()
+function Test:AddCommand_menuParamPositon_BelowMin()
   local out_of_bounds_args = Clone(_, valid_args)
   out_of_bounds_args.menuParams.position = -1
-  SendRpcExpectResponse(self, out_of_bounds_args)
+  SendAddCommandExpectResponse(self, out_of_bounds_args)
 end
 
-function Test:menuParamPositionOverMax()
+function Test:menuParamPosition_OverMax()
   local out_of_bounds_args = Clone(_, valid_args)
   out_of_bounds_args.menuParams.position = 1001
-  SendRpcExpectResponse(self, out_of_bounds_args)
+  SendAddCommandExpectResponse(self, out_of_bounds_args)
 end
 
-function Test:AddCommand_menuParamNameLengthOverMax()
+function Test:AddCommand_menuParamNameLength_OverMax()
   local out_of_bounds_args = Clone(_, valid_args)
   local menu_name_length_over_max = 501
   out_of_bounds_args.menuParams.menuName = common_functions:CreateString(menu_name_length_over_max)
-  SendRpcExpectResponse(self, out_of_bounds_args)
+  SendAddCommandExpectResponse(self, out_of_bounds_args)
 end
 
-function Test:AddCommand_vrCommandsArraySizeBelowMin()
+function Test:AddCommand_vrCommandsArraySize_BelowMin()
   local out_of_bounds_args = Clone(_, valid_args)
   out_of_bounds_args.vrCommands = {}
-  SendRpcExpectResponse(self, out_of_bounds_args)
+  SendAddCommandExpectResponse(self, out_of_bounds_args)
 end
 
-function Test:AddCommand_vrCommandsArraySizeOverMax()
+function Test:AddCommand_vrCommandsArraySize_OverMax()
   local out_of_bounds_args = Clone(_, valid_args)
   local vr_commands_array_size_over_max = 101
 
@@ -105,27 +117,27 @@ function Test:AddCommand_vrCommandsArraySizeOverMax()
   for i = 1, vr_commands_array_size_over_max do
     out_of_bounds_args.vrCommands[i] = table.concat({ i, "thVoiceCommand" })
   end
-  SendRpcExpectResponse(self, out_of_bounds_args)
+  SendAddCommandExpectResponse(self, out_of_bounds_args)
 end
 
-function Test:AddCommand_vrCommandLenghtOverMax()
+function Test:AddCommand_vrCommandLength_OverMax()
   local out_of_bounds_args = Clone(_, valid_args)
   local vr_command_length_over_max = 100
   out_of_bounds_args.vrCommands = { common_functions:CreateString(vr_command_length_over_max) }
-  SendRpcExpectResponse(self, out_of_bounds_args)
+  SendAddCommandExpectResponse(self, out_of_bounds_args)
 end
 
-function Test:AddCommand_cmdIconValueLengthOverMax()
+function Test:AddCommand_cmdIconValueLength_OverMax()
   local out_of_bounds_args = Clone(_, valid_args)
   local cmd_icon_value_length_over_max = 65536
   out_of_bounds_args.cmdIcon.value = common_functions:CreateString(cmd_icon_value_length_over_max)
-  SendRpcExpectResponse(self, out_of_bounds_args)
+  SendAddCommandExpectResponse(self, out_of_bounds_args)
 end
 
-function Test:AddCommand_imageTypeNotInEnum()
+function Test:AddCommand_imageType_NotInEnum()
   local out_of_bounds_args = Clone(_, valid_args)
   out_of_bounds_args.cmdIcon.imageType = "ANY"
-  SendRpcExpectResponse(self, out_of_bounds_args)
+  SendAddCommandExpectResponse(self, out_of_bounds_args)
 end
 -------------------------------------------Postcondition--------------------------------------
 common_steps:UnregisterApp("UnRegister_App", app.appName)
