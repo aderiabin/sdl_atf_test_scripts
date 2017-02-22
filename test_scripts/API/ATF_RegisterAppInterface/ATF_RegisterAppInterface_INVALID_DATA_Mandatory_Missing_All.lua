cb95fca03f1d980c676f512b746532f1ea202bc2
Andrii Pavliuk <arpavliuk@luxoft.com>
2017-02-22 14:10:01 +0200

    Add RegisterAppInterface INVALID_DATA Mandatory missing cases
    Added scripts for "RegisterAppInterface" RPC smoke testing when
    a request comes with mandatory params missing
    
    Task: APPLINK-32744
    Related: APPLINK-16115

---
+++ test_scripts/API/ATF_RegisterAppInterface/ATF_RegisterAppInterface_INVALID_DATA_Mandatory_Missing_All.lua
@@ -0,0 +1,42 @@
+-- Requirement summary: 
+--[APPLINK-16115][GeneralResultCodes]: INVALID_DATA mandatory parameters not provided
+
+-- Description:
+-- In case:
+---- the request comes without parameters defined as mandatory in mobile API
+-- SDL must:
+---- respond with resultCode "INVALID_DATA" and success: "false" value.
+
+-- Preconditions:
+-- 1. Connection, session and service #7 are initialized for the application
+-- 2. The request "RegisterAppInterface" is intended to be sent from mobile applicaton
+-- 3. The request doesn't contain any params
+
+-- Steps:
+-- 1. Mob -> SDL: "RegisterAppInterface" without any params
+
+-- Expected result:
+-- SDL -> Mob: success = false, resultCode = "INVALID_DATA"
+--------------------------------------------------------------------------------------------
+
+require('user_modules/all_common_modules')
+local consts = require('user_modules/consts')
+
+--[[ Local Variables ]]
+local rai_rpc_args = {}
+---------------------------------------------------------------------------------------------
+
+--[[ Preconditions ]]
+common_steps:PreconditionSteps("Start_SDL_To_Add_Mobile_Session", 5)
+---------------------------------------------------------------------------------------------
+
+--[[ Test ]]
+function Test:TestStep_RegisterAppInterface_Mandatory_Missing_All()
+  local cor_id = self.mobileSession:SendRPC("RegisterAppInterface", rai_rpc_args)
+  self.mobileSession:ExpectResponse(cor_id, { success = false, resultCode = "INVALID_DATA" })
+    :Timeout(consts.sdl_to_mobile_default_timeout)
+end
+---------------------------------------------------------------------------------------------
+
+--[[ Postconditions ]]
+common_steps:StopSDL("Postcondition_StopSDL")
