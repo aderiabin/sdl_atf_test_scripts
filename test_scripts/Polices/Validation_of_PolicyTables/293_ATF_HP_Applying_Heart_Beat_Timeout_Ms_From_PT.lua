4e69cdd2c0c0079708d5a9a7bb68585e96dc44fc
DongTaThanh <ttdong@luxoft.com>
2017-03-06 12:39:15 +0700

    Fix wrong indent and remove duplicate code
    - Fix wrong indent due to error of indent tool
    - Remove config.defaultProtocolVersion = 3
    - Remove --config.defaultProtocolVersion = 2

--- test_scripts/Polices/Validation_of_PolicyTables/293_ATF_HP_Applying_Heart_Beat_Timeout_Ms_From_PT.lua
+++ test_scripts/Polices/Validation_of_PolicyTables/293_ATF_HP_Applying_Heart_Beat_Timeout_Ms_From_PT.lua
@@ -22,7 +22,6 @@
 --[[ General configuration parameters ]]
 config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
 --Heartbeat is supported after protocolversion 3
---config.defaultProtocolVersion = 2
 config.defaultProtocolVersion = 3
 
 --[[ Required Shared libraries ]]
@@ -30,7 +29,7 @@ local commonFunctions = require ('user_modules/shared_testcases_genivi/commonFun
 local commonSteps = require ('user_modules/shared_testcases_genivi/commonSteps')
 local testCasesForPolicyTable = require ('user_modules/shared_testcases_genivi/testCasesForPolicyTable')
 local commonPreconditions = require ('user_modules/shared_testcases_genivi/commonPreconditions')
-config.defaultProtocolVersion = 3
+
 --[[ Local Variables ]]
 local time_prev = 0
 local time_now = 0
@@ -52,104 +51,107 @@ require('user_modules/AppTypes')
 local mobile_session = require('mobile_session')
 local events = require('events')
 local constants = require('protocol_handler/ford_protocol_constants')
-config.defaultProtocolVersion = 3
+
 --[[ Local Functions ]]
 local function DelayedExp(time)
   local event = events.Event()
   event.matches = function(self, e) return self == e end
   EXPECT_EVENT(event, "Delayed event")
   :Timeout(time+1000)
+
   RUN_AFTER(function()
       RAISE_EVENT(event, event)
-      end, time)
+    end,
+    time)
+end
+
+function Test:Precondition_ConnectMobile_FirstLifeCycle()
+  self:connectMobile()
+end
+
+function Test:Precondition_StartSession()
+  self.mobileSession = mobile_session.MobileSession(self, self.mobileConnection)
+  self.mobileSession:StartService(7)
+end
+
+function Test:Precondition_Register_Activate_Consent_App_And_Update_Policy_With_heart_beat_timeout_ms_Param()
+  local correlationId = self.mobileSession:SendRPC("RegisterAppInterface", config.application1.registerAppInterfaceParams)
+  EXPECT_HMINOTIFICATION("BasicCommunication.OnAppRegistered")
+  :Do(function(_,data)
+      self.HMIAppID = data.params.application.appID
+    end)
+  self.mobileSession:ExpectResponse(correlationId, { success = true, resultCode = "SUCCESS" })
+  self.mobileSession:ExpectNotification("OnHMIStatus", {hmiLevel = "NONE", audioStreamingState = "NOT_AUDIBLE", systemContext = "MAIN"})
+end
+
+--[[ Test ]]
+function Test:TestStep_GetAndCheck_HeartBeat_Time()
+  local index = 0
+  local event = events.Event()
+  event.matches = function(_, data)
+    return data.frameType == 0 and
+    (data.serviceType == 0) and
+    (data.frameInfo == 0) --HeartBeat
   end
+  print("No heartbeat yet")
 
-  function Test:Precondition_ConnectMobile_FirstLifeCycle()
-    self:connectMobile()
-  end
-
-  function Test:Precondition_StartSession()
-    self.mobileSession = mobile_session.MobileSession(self, self.mobileConnection)
-    self.mobileSession:StartService(7)
-  end
-
-  function Test:Precondition_Register_Activate_Consent_App_And_Update_Policy_With_heart_beat_timeout_ms_Param()
-    local correlationId = self.mobileSession:SendRPC("RegisterAppInterface", config.application1.registerAppInterfaceParams)
-    EXPECT_HMINOTIFICATION("BasicCommunication.OnAppRegistered")
-    :Do(function(_,data)
-        self.HMIAppID = data.params.application.appID
-      end)
-    self.mobileSession:ExpectResponse(correlationId, { success = true, resultCode = "SUCCESS" })
-    self.mobileSession:ExpectNotification("OnHMIStatus", {hmiLevel = "NONE", audioStreamingState = "NOT_AUDIBLE", systemContext = "MAIN"})
-  end
-
-  --[[ Test ]]
-  function Test:TestStep_GetAndCheck_HeartBeat_Time()
-    local index = 0
-    local event = events.Event()
-    event.matches = function(_, data)
-      return data.frameType == 0 and
-      (data.serviceType == 0) and
-      (data.frameInfo == 0) --HeartBeat
-    end
-    print("No heartbeat yet")
-
-    self.mobileSession:ExpectEvent(event, "Heartbeat")
-    :Do(function()
-        index = index + 1
-        print("HB[" .. index .. "] occurs at " .. tostring(timestamp()))
-        local HB_interval = 0
-        if index == 1 then
-          time_prev = timestamp()
-        elseif index == 2 then
-          time_now = timestamp()
-          HB_interval = time_now - time_prev
-          HBTime_min = HB_interval
-          HBTime_max = HB_interval
-          print("HB_interval: " .. tostring(HB_interval))
-        else
-          time_prev = time_now
-          time_now = timestamp()
-          HB_interval = time_now - time_prev
-          print("HB_interval: " .. tostring(HB_interval))
-          if HB_interval < HBTime_min then
-            HBTime_min = HB_interval
-          end
-          if HB_interval > HBTime_max then
-            HBTime_max = HB_interval
-          end
-        end
-
-        self.mobileSession:Send(
-          { frameType = constants.FRAME_TYPE.CONTROL_FRAME,
-            serviceType = constants.SERVICE_TYPE.CONTROL,
-            frameInfo = constants.FRAME_INFO.HEARTBEAT_ACK
-          }
-        )
-        end):Times(AtLeast(1))
-
-      DelayedExp(20000)
-    end
-
-    function Test:TestStep_Check_HeartBeat_Time()
-      -- Check HBTime_min and HBTime_max are around 1000 ms [700 - 1300].
-      if (HBTime_min > 700) and (HBTime_max < 1300) and (HBTime_min < HBTime_max) then
-        print(" HearBeat is in range ["..HBTime_min.." ; "..HBTime_max.."]ms , expected HeartBeat is 1000 ms")
+  self.mobileSession:ExpectEvent(event, "Heartbeat")
+  :Do(
+    function()
+      index = index + 1
+      print("HB[" .. index .. "] occurs at " .. tostring(timestamp()))
+      local HB_interval = 0
+      if index == 1 then
+        time_prev = timestamp()
+      elseif index == 2 then
+        time_now = timestamp()
+        HB_interval = time_now - time_prev
+        HBTime_min = HB_interval
+        HBTime_max = HB_interval
+        print("HB_interval: " .. tostring(HB_interval))
       else
-        self:FailTestCase("Wrong HearBeat time! Expected: 1000 ms, Actual: HBTime_min = "..HBTime_min..", HBTime_max = " .. HBTime_max .. " ms ")
+        time_prev = time_now
+        time_now = timestamp()
+        HB_interval = time_now - time_prev
+        print("HB_interval: " .. tostring(HB_interval))
+        if HB_interval < HBTime_min then
+          HBTime_min = HB_interval
+        end
+        if HB_interval > HBTime_max then
+          HBTime_max = HB_interval
+        end
       end
+
+      self.mobileSession:Send(
+        { frameType = constants.FRAME_TYPE.CONTROL_FRAME,
+          serviceType = constants.SERVICE_TYPE.CONTROL,
+          frameInfo = constants.FRAME_INFO.HEARTBEAT_ACK
+        }
+      )
+      end):Times(AtLeast(1))
+
+    DelayedExp(20000)
+  end
+
+  function Test:TestStep_Check_HeartBeat_Time()
+    -- Check HBTime_min and HBTime_max are around 1000 ms [700 - 1300].
+    if (HBTime_min > 700) and (HBTime_max < 1300) and (HBTime_min < HBTime_max) then
+      print(" HearBeat is in range ["..HBTime_min.." ; "..HBTime_max.."]ms , expected HeartBeat is 1000 ms")
+    else
+      self:FailTestCase("Wrong HearBeat time! Expected: 1000 ms, Actual: HBTime_min = "..HBTime_min..", HBTime_max = " .. HBTime_max .. " ms ")
     end
+  end
 
-    --[[ Postconditions ]]
-    commonFunctions:newTestCasesGroup("Postconditions")
-    testCasesForPolicyTable:Restore_preloaded_pt()
+  --[[ Postconditions ]]
+  commonFunctions:newTestCasesGroup("Postconditions")
+  testCasesForPolicyTable:Restore_preloaded_pt()
 
-    function Test.Postcondition_Restore_INI_file()
-      commonPreconditions:RestoreFile("smartDeviceLink.ini")
-    end
+  function Test.Postcondition_Restore_INI_file()
+    commonPreconditions:RestoreFile("smartDeviceLink.ini")
+  end
 
-    function Test.Postcondition_StopSDL()
-      StopSDL()
-    end
+  function Test.Postcondition_StopSDL()
+    StopSDL()
+  end
 
-    return Test
+  return Test
