8772a6293815a5a0a76b9b5afeca9bfb802753d7
Hoang Quang Nghi <hqnghi@luxoft.com>
2017-03-22 16:51:45 +0700

    Add files for hmiZoneCapabilities param of UI.GetCapabilities (#168)
    * Add common file for UIGetCapabilities
    Add files for hmiZoneCapabilities param
    
    * update full stable file
    
    * Update app and appName to use from const file
    Update code style
    Update const file
    
    * Add cases to run on CI
    
    * Update file Common to Add default value
    UPdate all scripts file to refer to common file
    
    * Update param name of all files

---
+++ test_scripts/Capabilities/UI-GetCapabilities/hmiZoneCapabilities/ATF_UI_GetCapabilities_hmiZoneCapabilities_BACK.lua
@@ -0,0 +1,49 @@
+---------------------------------------------------------------------------------------------
+-- Requirement summary:
+-- [APPLINK-23626]: response from HMI and RegisterAppInterface
+
+-- Description:
+-- SDL will send value BACK of UI.GetCapabilities.hmiZoneCapabilities from HMI to mobile
+
+-- Preconditions:
+-- 1. HMI -> SDL: value BACK of UI.GetCapabilities.hmiZoneCapabilities
+
+-- Steps:
+-- 1. Register App
+
+-- Expected result:
+-- SDL -> Mob: value BACK of UI.GetCapabilities.hmiZoneCapabilities
+---------------------------------------------------------------------------------------------
+require('user_modules/all_common_modules')
+
+--[[ Local variables]]
+local functions_UIGetCapabilities = require('test_scripts/Capabilities/UI-GetCapabilities/common_functions_for_UI_GetCapabilities')
+
+--[[ Preconditions ]]
+common_steps:AddNewTestCasesGroup("Preconditions")
+common_steps:PreconditionSteps("Preconditions", 2)
+function Test:Preconditions_InitHMI_OnReady()
+  functions_UIGetCapabilities:InitHMI_onReady_without_UI_GetCapabilities(self)
+
+  EXPECT_HMICALL("UI.GetCapabilities")
+  :Do(function(_,data)
+      send_hmi_capabilities.hmiZoneCapabilities = "BACK"
+      self.hmiConnection:SendResponse(data.id, "UI.GetCapabilities", "SUCCESS", send_hmi_capabilities)
+    end)
+end
+
+common_steps:AddMobileConnection("Preconditions_Add_Mobile_Connection")
+common_steps:AddMobileSession("Preconditions_Add_Mobile_Session")
+
+--[[ Test ]]
+common_steps:AddNewTestCasesGroup("Tests")
+function Test:Register_App()
+  local cid = self.mobileSession:SendRPC("RegisterAppInterface", const.default_app)
+  EXPECT_HMINOTIFICATION("BasicCommunication.OnAppRegistered", {application = {appName = const.default_app_name}})
+  EXPECT_RESPONSE(cid, {success = true, resultCode = "SUCCESS", hmiZoneCapabilities = {"BACK"}})
+  EXPECT_NOTIFICATION("OnHMIStatus", {systemContext = "MAIN", hmiLevel = "NONE", audioStreamingState = "NOT_AUDIBLE"})
+end
+
+--[[ Postconditions ]]
+common_steps:AddNewTestCasesGroup("Postconditions")
+common_steps:StopSDL("Postconditions_Stop_SDL")
